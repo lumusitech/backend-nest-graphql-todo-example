be78e806028a2a093180692a90acc100b6b77e8a
Luciano Figueroa <lumusitech@gmail.com>
2025-10-25 22:47:48 -0300

    chore: add initial config and test graphql endpoint helloWorld

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { HelloWorldResolver } from './hello-world/hello-world.resolver';
+import { HelloworldModule } from './helloworld/helloworld.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      // debug: false,
+      playground: false, //! Deprecated, use graphiql: true, or false if you use apollo studio
+      graphiql: false, // For use with Apollo Studio
+      plugins: [ApolloServerPluginLandingPageLocalDefault()], // Activate Apollo Studio landing page
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    HelloworldModule,
+  ],
+  controllers: [],
+  providers: [HelloWorldResolver],
 })
 export class AppModule {}
