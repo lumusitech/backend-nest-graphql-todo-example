f64fcad9274eeba342d0206a86e256c911ffb0a4
Luciano Figueroa <lumusitech@gmail.com>
2025-10-25 23:30:36 -0300

    feat: add basic query methods to test

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,28 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Int, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, {
+    name: 'hello',
+    description: 'A simple Hello World query',
+  })
   helloWorld(): string {
     return 'Hello World!';
+  }
+
+  @Query(() => Float, {
+    name: 'randomNumber',
+    description: 'Get a random float number between 0 and 1',
+  })
+  getRandom(): number {
+    return Math.random() * 100;
+  }
+
+  @Query(() => Int, {
+    name: 'randomFromZeroTo',
+    description: 'Get a random integer number between 0 and 9',
+  })
+  getRandomFromZeroTo(): number {
+    return Math.floor(Math.random() * 10);
   }
 }
